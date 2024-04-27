2d9d3353610f705c8057f039522207b5df7af59a
Wilhelm Thieme <wjthieme@gmail.com>
2024-04-27 11:07:13 -0400

    Tweak preview image

--- app/api/preview/route.tsx
+++ app/api/preview/route.tsx
@@ -40,16 +40,13 @@ export async function GET(): Promise<ImageResponse> {
 
   const element =
     <div tw="w-full h-full bg-emerald-900 text-slate-200 flex flex-col justify-center">
-      <div tw="mx-auto text-6xl font-black">
-        jewl
-      </div>
-      <div tw="mx-auto text-6xl font-black">
+      <div tw="mx-10 text-6xl font-black">
         Tax-efficient on-chain renumeration.
       </div>
       <div tw="mx-10 my-4 text-sm font-medium">
         Decrease your tax liability on bonuses and equity compensation by up to 50%.
         Prevent forced sales of your assets due to tax obligations.
-        Get started with jewl today.
+        Start using jewl today.
       </div>
     </div>;
   return new ImageResponse(element, { width, height, fonts });
